0ee086a985d34f6c38b9405fa134a88e4e46b491
AdrianoFonseca <adrianofonseca@poli.ufrj.b>
2020-12-08 23:11:38 -0300

    added update function for diode stamp

--- src/components/component.py
+++ src/components/component.py
@@ -4,6 +4,7 @@ class Component():
         self.value = value
         self.nodes = nodes
         self.type = None
+        self.linear = True
 
     def __repr__(self) -> str:
         _str = f"Component {self.id} of type {self.type} and value {self.value} between nodes {self.nodes}"
--- src/components/resistor.py
+++ src/components/resistor.py
@@ -1,4 +1,5 @@
 from .component import Component
+import numpy as np
 
 class Resistor(Component):
     def __init__(self, id, nodes, value):
@@ -18,7 +19,42 @@ class Diode(Component):
         super().__init__(id, nodes, js)
         self.type = 'Diode'
         self.js = js
-        self.n = n
-        self.vt = vt
+        self.vt = n*vt
+        self.linear = False
+
+        self.vd = 0.6
+        self.G = self.diodederiv()
+        self.I = self.diodefunc() - self.G*self.vd
+
+
+    def stamp(self,circuitA,circuitb,add = 1):
+
+        G = self.G * add
+        I = self.G * add
+        
+        circuitA[self.nodes[0],self.nodes[0]] += G
+        circuitA[self.nodes[0],self.nodes[1]] -= G
+        circuitA[self.nodes[1],self.nodes[0]] -= G
+        circuitA[self.nodes[1],self.nodes[1]] += G  
+
+        circuitb[self.nodes[0]] -= I
+        circuitb[self.nodes[1]] += I
+
+    def update_biaspoint(self,circuitE, circuitA,circuitb):
+        circuitE = [0] + circuitE
+        vd = circuitE[self.node[0]] - circuitE[self.node[1]]
+
+        self.stamp(self,circuitA,circuitb, -1)
+        
+        self.G = self.diodederiv()
+        self.I = self.diodefunc() - self.G*self.vd
+
+        self.stamp(self,circuitA,circuitb)
+        
+    def diodefunc(self):
+	    return(self.js*(np.exp(self.vd/self.vt)-1))
+
+    def diodederiv(self):
+	    return((self.js/self.vt)*np.exp(self.vd/self.vt))
 
 
