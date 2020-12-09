867bb4d3dc7dbde20a72f8c8824cd27b469f1102
AdrianoFonseca <adrianofonseca@poli.ufrj.b>
2020-12-09 16:35:15 -0300

    Newton Raphson Fixes:  now using error tolerance for convergence as well and max iter

--- src/solver/dc_solver.py
+++ src/solver/dc_solver.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class DCSolver(Solver):
 
-    def __init__(self, elements_list, num_nodes):
+    def __init__(self, elements_list, num_nodes, max_iter = 200, tol = 1e-8):
         super().__init__(elements_list, num_nodes)
+        self.max_iter = max_iter
+        self.tol = tol
         self.update_system()
         self.circuitE[1:] = self.solve()
 
@@ -19,11 +21,14 @@ class DCSolver(Solver):
         return np.linalg.solve(self.circuitA[1:,1:],self.circuitb[1:])
 
     def NewtonRapshon(self):
-        for iter in range(200):
+        for iter in range(self.max_iter):
             for element in self.nonlinear:
                 element.update_biaspoint(self.circuitE, self.circuitA, self.circuitb)
 
+            temp = self.circuitE[1:].copy()
             self.circuitE[1:]  = self.solve()
+            if(np.sum((temp - self.circuitE[1:])**2) < self.tol):
+                break
             
     def debug(self):
         print(self.circuitA, self.circuitB)
